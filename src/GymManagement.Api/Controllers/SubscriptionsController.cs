191712251ffdef1029458c7d1ae666acfbab3b15
YurDen91 <yurden20@gmail.com>
2024-11-28 08:32:36 +0200

    add controller and http file for test

---
+++ src/GymManagement.Api/Controllers/SubscriptionsController.cs
@@ -0,0 +1,33 @@
+﻿using GymManagement.Contracts.Subscriptions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GymManagement.Api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class SubscriptionsController : ControllerBase
+{
+    private readonly ILogger<SubscriptionsController> _logger;
+
+    public SubscriptionsController(ILogger<SubscriptionsController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpPost]
+    public IActionResult CreateSubscription(CreateSubscriptionRequest request)
+    {
+        return Ok(request);
+    }
+
+    [HttpGet]
+    public IEnumerable<SubscriptionResponse> Get()
+    {
+        return new List<SubscriptionResponse>
+        {
+            new SubscriptionResponse(Guid.NewGuid(), SubscriptionType.Free),
+            new SubscriptionResponse(Guid.NewGuid(), SubscriptionType.Starter),
+            new SubscriptionResponse(Guid.NewGuid(), SubscriptionType.Pro)
+        };
+    }
+}
